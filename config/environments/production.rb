b6a6011bfcf6be802833f3509c93a5137d0a75ac
“Marksoloff” <marksoloff@gmail.com>
2019-08-14 01:23:26 -0500

    paths

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,7 +26,7 @@ Rails.application.configure do
 
   config.public_file_server.enabled = true
 
-  config.serve_static_assets = false
+  config.serve_static_assets = true
   config.assets.digest = true
 
   # >..<
