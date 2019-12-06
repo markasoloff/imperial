94f6b688a842946de77168e09a0839e3f079bf6a
“Marksoloff” <marksoloff@gmail.com>
2019-12-05 22:23:37 -0600

    custom font added

--- config/application.rb
+++ config/application.rb
@@ -27,7 +27,7 @@ module Imperial
         config.generators.helper = false
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-    
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
     config.assets.precompile << /\.(?:svg|eot|woff|ttf)\z/
 
     # Settings in config/environments/* take precedence over those specified here.
