2d21d36484aaa77dc947377a0366d2d061f20a69
markasoloff <marksoloff@gmail.com>
2020-02-13 20:56:52 -0600

    new about image/padding

---
+++ public/assets/soloff-tower-of-hanoi-3fdb8f5251af5d3789f2e79c035e710f1844de139f953bdcbbb6cc00d6fb629a.java
@@ -0,0 +1,51 @@
+/* Tower of Hanoi
+
+ 
+
+Problem Description:
+
+The candidate has to resolve "Tower of Hanoi" problem.
+
+When a program runs, it should ask for  "Number of Towers" (at least 5 max 10) & "Number of Disks" (max 40)
+
+The program should show each step in output. 
+
+ 
+Requirements: 
+1. Program asks user for "Number of Towers". They must select a number between 5 and 10 - error message for all other inputs.
+2. Program asks user for "Number of Disks". They must select a number between 3 and 40 - error message for 0 - 2 disks or any unsantized input.
+
+Evaluation Criteria:
+
+OOPs concept (10%)
+Coding standard (10%)
+Problem-solving skill (80%)
+
+
+The full requirements:
+
+Must correctly solve the required motion without violating stacking rules (e.g. on each peg, no bigger disk sits on any smaller disk).
+
+5 - 10 towers / pegs
+3 - 40 disks
+Not required but preferred:
+
+Good software engineering practices (e.g. code formatting, variable names, comments / JavaDoc, unit tests)
+Efficiency of moves (no unneeded moves)
+Efficiency of code (time and space complexity)  At least be able to estimate general space/time complexity.
+
+
+
+questions:
+What is JavaDoc?
+How do I apply unit tests to a java program?
+How do I format Java?
+How do I comment in Java?
+How do I name variables in Java?
+How do I determine the efficiency of my program/'s' moves?
+
+What is the time and space complexity of my program? 
+How do I estimate time and space complexity?
+
+
+*/
